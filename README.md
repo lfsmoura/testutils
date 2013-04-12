8b044240a3bd25e439b440ed36c3bba940274bad
lfsmoura <leofsmoura@gmail.com>
2013-04-12 19:03:32 -0300

    new options

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ Utils
 
 	. testutil.sh -n 10 "./a.out"
 
-> executes the command line "./a.out" ten times, the output of the program must be of the format
+executes the command line "./a.out" ten times, the output of the program must be of the format
 
 	field:value
 
-> for example if your program outputs
+for example if your program outputs
 
 	runningtime:10s
 	iter:5
@@ -21,9 +21,17 @@ Utils
 	runningtime:12s
 	iter:6
 
-> testutil output is going to be
+testutil output is going to be
 
-	runningtime	iter
-	10		5
-	20		10
-	12		6
+>runningtime      iter
+>10               5
+>20               10
+>12               6
+
+options:
+-h help
+-m cmd - takes indices from paramter ex. -m "seq 5 10"
+-l     - adds a "label" # to the results with the indices of the calls
+-p     - uses the indices as the last paramater of cmd
+-n     - same as -m "seq 1 n", executes n times
+ 
--- testutil.sh
+++ testutil.sh
@@ -2,41 +2,42 @@
 
 n=1
 
-while [ "$1" ]
+while getopts nm:lph opt
 do
-  echo $1
-  case $1 in
-    "-h"|"--help")
+  case $opt in
+    h|[?]|help)
       echo "./testeutil [OPTIONS] 'cmd'"
       echo " -n repeat cmd n times "
       echo "ex: ./testeutil -n 10 'echo test:1'"
       exit
     ;;
-  "-n")
-      shift
-      n=$1
+  n)
+      n=$OPTARG
     ;;
-  "-p")
+  p)
       use_n_as_parameter="true"
   ;;
-  "-m")
-      shift
-      fun=$1
+  m)
+    echo $OPTARG
+      fun=$OPTARG
   ;;
-    *) 
-      cmd1=$1
-    ;;
+  l)
+    label_with_n="true"
+  ;;
   esac
-  shift
 done
+
+shift $((OPTIND-1))
+cmd1=$1
 
 if [ -z "$fun" ]; then  fun="seq 1 $n"; fi
 
 touch $$temp
 for i in $($fun)
 do
-    cmd=$cmd1
-    if [ "$use_n_as_parameter" ]; then cmd=$cmd" "$i; fi
+  cmd=$cmd1
+  if [ "$label_with_n" ]; then echo "#:$i" >> $$temp; fi
+  if [ "$use_n_as_parameter" ]; then cmd=$cmd" "$i; fi
   $cmd >> $$temp
 done
 
@@ -45,6 +46,7 @@ awk -F ":" '
   BEGIN{ fields=0; maxcount=0}
   NF>1 {
     if(count[$1] == 0) {
+      maxsize[$1] = length($1);
       count[$1] = 0;
       fieldNames[fields]=$1
       fields += 1;
@@ -54,16 +56,18 @@ awk -F ":" '
     count[$1] += 1;
     if(count[$1] > maxcount)
       maxcount = count[$1];
+    if(length($2) > maxsize[$1])
+      maxsize[$i] = length($2)
   }
   END{
     for(i = 0; i < fields-1; i++)
-      printf("%s \t", fieldNames[i]); 
-    print fieldNames[fields-1];
+      printf("%*s \t", -maxsize[fieldNames[i]], fieldNames[i]); 
+    printf("%*s \n", -maxsize[fieldNames[fields-1]], fieldNames[fields-1]);
 
     
     for(i = 0; i < maxcount; i++) {
       for(j = 0; j < fields; j++)
-        printf("%s\t", values[fieldNames[j], i]);
+        printf("%*s\t", -maxsize[fieldNames[j]], values[fieldNames[j], i]);
       print ""
     }
   }

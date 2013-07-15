702254566fa7bda9f5e5cad6137154314a3554df
Leonardo Moura <leofsmoura@gmail.com>
2013-07-15 11:21:44 -0300

    install cpplint

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 cp testrun.sh /usr/local/bin/trun
 cp testformat.sh /usr/local/bin/tformat
 cp suffix.sh /usr/local/bin/suffix
+
+wget http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py -O /usr/local/bin/cpplint
+chmod +x /usr/local/bin/cpplint
